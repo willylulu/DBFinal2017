09a782a743d8fe8035914d6870f4240d83dbd776
willylulu <maya6282@gmail.com>
2017-05-19 23:11:52 +0800

    give some example and frontend test

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
+//under this line is the rest api backend part
+
+app.get('/peoples', function(request, response) {
+    var peoples = [{
+            name: "dick",
+            age: 28,
+            gender: "male",
+            money: 1000000
+        },
+        {
+            name: "Feng",
+            age: 21,
+            gender: "male",
+            money: -10000000
+        },
+        {
+            name: "Jean",
+            age: 21,
+            gender: "male",
+            money: 1.25E10
+        },
+    ];
+    response.send(peoples);
+});
+
+app.post('/text', function(request, response) {
+    var data = request.body;
+    var text = data.text;
+    response.send("You send text " + text);
+});
