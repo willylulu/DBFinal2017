009e80b1498e09fc5a65a0f24eb5b15cba0f1d71
willylulu <maya6282@gmail.com>
2017-05-19 23:06:54 +0800

    Update app.json

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.sendFile(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
