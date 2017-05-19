30384bd4b9b1f562b27cb52cedf91bd0a83bc8cc
willylulu <maya6282@gmail.com>
2017-05-19 23:39:08 +0800

    fweew

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//使用express撰寫Rest API
 var express = require('express');
+
+//body-parser是用來parse post封包裡面的內容
 var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//設定封包解析格式為json
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json());
 
+//指定root頁面檔案
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
@@ -16,8 +21,18 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-//under this line is the rest api backend part
+// 用來和postgres連線的libary
+// var pg = require('pg');
+// var conString = "postgres://YourUserName:YourPassword@localhost:5432/YourDatabase";
+// var client = new pg.Client(conString);
+// client.connect();
+// client.query("INSERT INTO junk(name, a_number) values('Ted',12)");
+// var query = client.query("SELECT * FROM junk");
 
+//上面都可以不要動
+//以下是撰寫Rest API
+
+//當有人用Rest API get <網址>/peoples觸發函式
 app.get('/peoples', function(request, response) {
     var peoples = [{
             name: "dick",
@@ -38,11 +53,18 @@ app.get('/peoples', function(request, response) {
             money: 1.25E10
         },
     ];
+
+    //將json資料回傳
     response.send(peoples);
 });
 
+//當有人用Rest API post <網址>/text觸發函式
 app.post('/text', function(request, response) {
+
+    //擷取post封包內容
     var data = request.body;
     var text = data.text;
+
+    //將json資料回傳
     response.send("You send text " + text);
 });
